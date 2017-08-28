9ae8c492dd425029c77bc56f5771e2d5ddf52d39
Jake Ledoux <jaik.exe@gmail.com>
2017-08-28 13:59:18 -0400

    pypi changes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'applepie',
+  packages = ['applepie'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Homemade python library to handle text operations.',
+  author = 'Jake Ledoux',
+  author_email = 'jaik.exe@gmail.com',
+  url = 'https://github.com/NyanJaik/applepie', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['string','text','parsing','operations','yes or no','names'], # arbitrary keywords
+  classifiers = [],
+)
