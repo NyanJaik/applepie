a12c1fabada02ac8392b1643bf2cc1b68bd1ad99
Jake Ledoux <jaik.exe@gmail.com>
2017-08-28 15:43:26 -0400

    Adjustments and framework for tonumber function

--- applepie/__init__.py
+++ applepie/__init__.py
@@ -51,12 +51,107 @@ def gender( intext ):
 		return "?"
 
 def yesno( intext ):
-	if any(word in strip(intext) for word in data['yes']):
+	if any(word in strip(intext) for word in data['yes']) or strip(intext) == "y":
 		return True
-	elif any(word in strip(intext) for word in data['no']):
+	elif any(word in strip(intext) for word in data['no']) or strip(intext) == "n":
 		return False
 	else:
 		return "?"
+
+def tonumber( intext ):
+	final_exp_digits = 0
+	outtext = ""
+	intext = intext.lower().strip().translate({ord(i):" " for i in  "?!@#$%^&*',.:;+=/\|][}{-_("}).split()
+	tenifier = False
+	for i in intext:
+		if i == "and":
+			continue
+
+		suffix = ""
+
+		#Places
+		if i == "hundred":
+			exp_digits = 3
+		elif i == "thousand":
+			exp_digits = 4 if not tenifier else 5
+		elif i == "million":
+			exp_digits = 7 if not tenifier else 8
+		elif i == "billion":
+			exp_digits = 10 if not tenifier else 11
+		elif "ty" in i:
+			exp_digits = 2
+		else:
+			exp_digits = 0
+
+		#Numbers
+		if i in ['one']:
+			suffix = "1"
+		elif i in ['two','twenty']:
+			suffix = "2"
+		elif i in ['three','thirty']:
+			suffix = "3"
+		elif i in ['four', 'forty']:
+			suffix = "4"
+		elif i in ['five','fifty']:
+			suffix = "5"
+		elif i in ['six', 'sixty']:
+			suffix = "6"
+		elif i in ['seven','seventy']:
+			suffix = "7"
+		elif i in ['eight', 'eighty']:
+			suffix = "8"
+		elif i in ['nine','ninety']:
+			suffix = "9"
+
+		# Teens
+		if i == 'ten':
+			suffix = "10"
+			tenifier = True
+		elif i == 'eleven':
+			suffix = "11"
+			tenifier = True
+		elif i == 'twelve':
+			suffix = "12"
+			tenifier = True
+		elif i == 'thirteen':
+			suffix = "13"
+			tenifier = True
+		elif i == 'fourteen':
+			suffix = "14"
+			tenifier = True
+		elif i == 'fifteen':
+			suffix = "15"
+			tenifier = True
+		elif i == 'sixteen':
+			suffix = "16"
+			tenifier = True
+		elif i == 'seventeen':
+			suffix = "17"
+			tenifier = True
+		elif i == 'eighteen':
+			suffix = "18"
+			tenifier = True
+		elif i == 'nineteen':
+			suffix = "19"
+			tenifier = True
+		else:
+			tenifier = False
+
+		# Expected digits
+		if exp_digits > final_exp_digits:
+			final_exp_digits += exp_digits
+		else:
+			for i in range(final_exp_digits - len(outtext) - exp_digits):
+				outtext += "0"
+
+		outtext += suffix
+
+	# Extra zeros
+	for i in range(final_exp_digits-len(outtext)):
+		outtext += "0"
+
+	return outtext
+
 
 def main():
 	quit = False
@@ -66,6 +161,7 @@ def main():
 			quit == True
 		else:
 			print(yesno(intext))
+
 
 if __name__ == '__main__':
 	main()
